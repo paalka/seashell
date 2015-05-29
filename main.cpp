94bb7a93b6575ffa0f14e877deb6c5f5ae425927
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-05-29 21:39:01 +0200

    Add a working example of a command.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char **argv) {
     pid_t pid = fork();
+    cout << pid;
+    char *const parmList[] = {"/bin/ls", "-l", "/Users/naiad/", NULL};
+    execvp("ls", parmList);
     return 0;
 }
