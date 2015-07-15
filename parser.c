c5110f7cb772cfe11a61296c7983f925164a28fc
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-07-15 22:19:37 +0200

    Adds comments attempting to clearify how the function works.

--- parser.c
+++ parser.c
@@ -4,21 +4,31 @@
 #include <stdlib.h>
 
 char **tokenize_str(char command[], char delimiter[]) {
+    /*
+     * Divides the string 'command' into several strings.
+     * The string is divided on each position where the char 'delimiter' is
+     * found.
+     * Returns a array of strings.
+     */
+
     char **res = NULL;
     char *p = strtok(command, delimiter);
     int n_spaces = 0;
     int i = 0;
 
+    // Use strtok to obtain the tokens from the string.
     while (p) {
         n_spaces++;
         res = realloc(res, sizeof(char*) *n_spaces);
         check(res != NULL, "Mem allocation failed!");
         
         res[n_spaces-1] = p;
-        p = strtok (NULL, " ");
+        
+        // Pass NULL to obtain the next token.
+        p = strtok (NULL, " "); 
     }
-
-    /* realloc one extra element for the last NULL */
+    
+    // Increase the size of the array, so that the NULL char can be added.
     res = realloc(res, sizeof(char*) *(n_spaces+1));
     res[n_spaces] = 0;
 
