8833893b0c249464aa072b65a003b8a228fa069d
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-07-16 10:12:52 +0200

    Add a file which will be used to change the current directory of the shell.

---
+++ builtins/cd.c
@@ -0,0 +1,11 @@
+#include "../dbg.h"
+#include "../shell.h"
+
+int change_directory(struct Config current_shell, char *new_dir) {
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
