9a9d856fcecef8100ee80ac48aca32c71ca04bd8
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-07-16 10:10:53 +0200

    Refactor the main function to make it more modular.

--- shell.c
+++ shell.c
@@ -1,14 +1,21 @@
 #include "parser.h"
+#include "dbg.h"
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
+int execute_ext_command(char *file, char **params) {
+    int error_code;
+
+    // create a child process for the command to be executed.
     pid_t pid = fork();
-    //cout << pid;
-    char foo[] = "/bin/ls";
-    char *parmList = {"/bin"};
 
+    error_code = execvp(file, params);
+    check(error_code == 0, "Failed to execute command.");
+error:
+    // TODO: Clean up the child process.
+    return -1;
+}
 
-    execvp("ls", parmList);
+int main(int argc, char **argv) {
     return 0;
 }
