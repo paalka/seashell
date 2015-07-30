2e3da518eb8600602edd007f1cfc7ec14aa4c48d
Paul Karlsrud <paulkarlsrud@gmail.com>
2015-07-30 12:21:47 +0200

    Make the shell handle internal commands as well.

--- shell.c
+++ shell.c
@@ -1,28 +1,44 @@
 #include "parser.h"
+#include "builtin.h"
 #include "shell.h"
 #include "lib/dbg.h"
+
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
+int (*builtin_func[]) (char **) = {
+    &sea_cd,
+    &sea_help,
+    &sea_exit,
+};
+
 int execute_command(char **args)
 {
-    pid_t pid, wpid;
+
+    int builtin = is_builtin(args[0]);
     int status;
+    if (builtin == -1) {
+        pid_t pid, wpid;
 
-    pid = fork();
+        pid = fork();
 
-    if (pid == 0) {
-        status = execute_ext_command(args[0], args);
-    } else if (pid < 0) {
-        perror("seashell");
+        if (pid == 0) {
+            status = execute_ext_command(args[0], args);
+        } else if (pid < 0) {
+            perror("seashell");
+        } else {
+            do {
+                wpid = waitpid(pid, &status, WUNTRACED);
+            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        }
+
+        return 1;
     } else {
-        do {
-            wpid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        status = (*builtin_func[builtin])(args);
+        return 1;
     }
-
-    return 1;
 }
 
 int execute_ext_command(char *file, char **args)
